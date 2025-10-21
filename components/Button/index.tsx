aa88f4311972ed19f4760d3a53cfee088788fca2
Chrigor <chrigor@evnts.com.br>
2025-10-21 10:51:23 -0300

    feat: add docs for button and intro

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -1,4 +1,6 @@
-export default function Button(props: React.ButtonHTMLAttributes<HTMLButtonElement>) {
+export type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
+
+export default function Button(props: ButtonProps) {
   return (
     <button {...props} className="rounded-[8px] bg-bg-button text-white cursor-pointer px-[32px] py-[12px]"/>
   );
