3620f415c21278af07bdb678ca3b29ba6aa06a8e
Chrigor <chrigor.edu3@gmail.com>
2025-10-25 20:35:24 -0300

    feat: add configs for npm

---
+++ components/index.ts
@@ -0,0 +1,5 @@
+export { default as Button, type ButtonProps } from "./Button";
+
+export { type LinkProps, default as Link } from "./Link";
+
+export { type TypographyProps, default as Typography } from "./Typography";
